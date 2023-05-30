63e4f270dadce9efc024dd4b899a7903e4975fc0
Alex Lirola Fenoy <alirola-@c1r7s6.42malaga.com>
2023-05-30 18:41:47 +0200

    add files

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alirola- <alirola-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 17:37:45 by alirola-          #+#    #+#             */
+/*   Updated: 2023/05/30 17:37:45 by alirola-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+#endif
+
+char	*get_next_line(int fd);
+char 	*ft_strjoin(char const *s1, char const *s2);
+void  	*ft_calloc(size_t num, size_t size);
+int		ft_strlen(const char *str);
+int 	ft_strchr(char *str, int c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alirola- <alirola-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 17:41:43 by alirola-          #+#    #+#             */
+/*   Updated: 2023/05/30 18:34:14 by alirola-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*ret_str;
+	size_t	len;
+	int		i;
+	int		a;
+
+	i = 0;
+	a = 0;
+	len = ((size_t)ft_strlen(s1) + (size_t)ft_strlen(s2));
+	ret_str = (char *)malloc(((len + 1) * sizeof(char)));
+	if (!ret_str)
+		return (NULL);
+	while (s1[i])
+	{
+		ret_str[i] = s1[i];
+		i++;
+	}
+	while (s2[a])
+	{
+		ret_str[i + a] = s2[a];
+		a++;
+	}
+	ret_str[i + a] = '\0';
+	return (ret_str);
+}
+
+void	ft_bezero_full(void *s, size_t n)
+{
+	unsigned char	*b;
+	size_t			i;
+
+	i = 0;
+	b = (unsigned char *)s;
+	while (i < n)
+	{
+		b[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (0);
+	ft_bezero_full(ptr, count * size);
+	return (ptr);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_strchr_mod(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
